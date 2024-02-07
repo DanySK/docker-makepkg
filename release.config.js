e59b19d2c2fa9498bd4ff2be9b7c8938eae6f259
Danilo Pianini <danilo.pianini@gmail.com>
2024-02-07 18:48:05 +0100

    configure semantic-release

---
+++ release.config.js
@@ -0,0 +1,16 @@
+var publishCmd = `
+docker build -t "$IMAGE_NAME:\${nextRelease.version}" .
+docker push --all-tags "$IMAGE_NAME"
+`
+var config = require('semantic-release-preconfigured-conventional-commits');
+config.plugins.push(
+    [
+        "@semantic-release/exec",
+        {
+            "publishCmd": publishCmd,
+        }
+    ],
+    "@semantic-release/github",
+    "@semantic-release/git",
+)
+module.exports = config
